0dce3a6c72ef4021b300f6d9ea4eb7968e52eadc
harsh27d <harshdeepdamodhar2004@gmail.com>
2026-09-11 15:42:10 +0530

    add test7.py

---
+++ test7.py
@@ -0,0 +1,17 @@
+num = int(input("Enter a number: "))
+if num==0:
+ print("the day is monday")
+elif num==1:
+ print("the day is tuesday")
+elif num==2:
+ print("the day is wednesday")
+elif num==3:
+ print("the day is thursday")
+elif num==4:
+ print("the day is friday")
+elif num==5:
+ print("the day is saturday")
+elif num==6:
+ print("the day is sunday")
+else:
+ print("Enter a valid number between 0 to 6")
